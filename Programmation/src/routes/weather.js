352ae7633e264841c4d913b277de14cb32cb3995
Kushiie <riviere.lisa1875@gmail.com>
2023-04-14 16:32:25 +0200

    #19

--- Programmation/src/routes/weather.js
+++ Programmation/src/routes/weather.js
@@ -41,17 +41,17 @@ router.get('/rain', (_, res) => {
 
 /**
  * @swagger
- * /weather/freeze:
+ * /weather/temperatures:
  *   get:
- *     summary: Renvoie les prévisions en fonction de la température si elle descends à 0
- *     description: Récupère les prévisions de la température pour voir si elle descends à 0 ou en dessous
+ *     summary: Renvoie les températures sur 24h
+ *     description: Récupère les températures sur 24h
  *     responses:
  *       200:
- *         description: Prévisions de la température récupérée avec succès
+ *         description: Prévisions de la température sur 24h récupérée avec succès
  *       403:
  *         description: Erreur lors de la requête API à OpenWeatherMap
 */
-router.get('/freeze', (_, res) => {
+router.get('/temperatures', (_, res) => {
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Valbonne&appid=${process.env.weatherAPIKey}`)
         .then(res => res.json())
@@ -60,20 +60,14 @@ router.get('/freeze', (_, res) => {
             const allTemperatures = [];
 
             data.list.slice(0, 8).forEach(item => {
-
-                const temperature = Math.round(item.main.temp - 273.15);
-
-                if(temperature <= 0) {
-                    return allTemperatures.push({
-                        date: new Date(item.dt_txt).toLocaleString(),
-                        description: item.weather[0].description,
-                        temperature: temperature
-                    });
-                };
+                return allTemperatures.push({
+                    date: new Date(item.dt_txt).toLocaleString(),
+                    description: item.weather[0].description,
+                    temperature: Math.round(item.main.temp - 273.15)
+                });
             });
 
-            if (allTemperatures.length > 0) return res.json({ status: 200, willFreeze: true, temperatures: allTemperatures });
-            else return res.json({ status: 200, willFreeze: false, temperatures: [] });
+            return res.json({ status: 200, temperatures: allTemperatures });
         })
         .catch((err) => res.json({ status: 403, error: err }));
 })
