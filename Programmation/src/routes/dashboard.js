440007f0dfe2907484ce498759fb557692fc885d
Kushiie <riviere.lisa1875@gmail.com>
2023-05-05 15:49:32 +0200

    #30

--- Programmation/src/routes/dashboard.js
+++ Programmation/src/routes/dashboard.js
@@ -1,7 +1,8 @@
 import users from '../database/users.json' assert { type: 'json' };
+import { checkAuth } from '../utils.js';
 import { Router } from "express";
 const router = Router();
 
-router.get('/home', (req, res) => res.render('dashboard/home', { connectedUser: users.find((user) => user.username === req.session.username) }));
+router.get('/home', checkAuth, (req, res) => res.render('dashboard/home', { connectedUser: users.find((user) => user.username === req.session.username) }));
 
 export default router;
