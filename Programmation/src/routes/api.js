d037bcfacdf3c64148f3bdb5af8a5f43b6d22fde
Kushiie <riviere.lisa1875@gmail.com>
2023-05-05 15:50:01 +0200

    #27

--- Programmation/src/routes/api.js
+++ Programmation/src/routes/api.js
@@ -5,10 +5,6 @@ const router = Router();
 
 import weatherRoute from '../routes/weather.js';
 
-router.get('/', (_, res) => res.render('api/home'));
-
-router.get('/endpoints', (_, res) => res.render('api/endpoints'));
-
 router.use('/weather', weatherRoute);
 
 router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
